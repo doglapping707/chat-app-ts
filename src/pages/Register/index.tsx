3893f4d9081a390749e35fe2a0d0826a92aa694f
doglapping707 <doglapping707@doglapping707noMacBook-Pro.local>
2024-03-02 10:35:35 +0900

    アカウント登録とログインページのデザインを追加

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,5 +1,14 @@
 export default function Register() {
     return (
-        <h1>アカウント作成画面</h1>
+        <div className="form_container">
+            <h1>Register</h1>
+            <form action="">
+                <input type="text" placeholder="Display Name" />
+                <input type="text" placeholder="Email" />
+                <input type="text" placeholder="Password" />
+                <button>Sign up</button>
+            </form>
+            <p>You do have an account?<a href="#">Login</a></p>
+        </div>
     )
 }
